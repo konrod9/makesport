f139a070ec9729e6df479686ed16ab3dfbf858ca
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 13:52:39 +0700

    di: Register services at DI

---
+++ backend/src/MakeSport.Application.DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MakeSport.Application.UseCases.CreateVenue;
+using MakeSport.Application.UseCases.GetVenues;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakeSport.Application.DI;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddVenuesServices(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IGetVenuesUseCase, GetVenuesUseCase>()
+            .AddScoped<ICreateVenueUseCase, CreateVenueUseCase>();
+
+        return services;
+    }
+}
