5963ec8a6b4104a74af7afea11857000d0e158c2
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 11:28:18 +0700

    feat: Add VenueDbContext

---
+++ backend/src/MakeSport.Infrastructure.Postgres/VenueDbContext.cs
@@ -0,0 +1,9 @@
+﻿using MakeSport.Domain.Venues;
+using Microsoft.EntityFrameworkCore;
+
+namespace MakeSport.Infrastructure.Postgres;
+
+public class VenueDbContext(DbContextOptions<VenueDbContext> options) : DbContext(options)
+{
+    public DbSet<Venue> Venue { get; set; }
+}
