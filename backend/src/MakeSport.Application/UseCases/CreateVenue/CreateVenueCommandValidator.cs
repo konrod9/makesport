5c3f7474f2b8c174639132eb9d34be537970c341
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 19:07:18 +0700

    feat: Validator for create venue command

---
+++ backend/src/MakeSport.Application/UseCases/CreateVenue/CreateVenueCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MakeSport.Application.Exceptions;
+
+namespace MakeSport.Application.UseCases.CreateVenue;
+
+public class CreateVenueCommandValidator : AbstractValidator<CreateVenueCommand>
+{
+    public CreateVenueCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithErrorCode(ValidationErrorCode.Empty)
+            .MaximumLength(50).WithErrorCode(ValidationErrorCode.TooLong);
+        
+        RuleFor(c => c.Description)
+            .MaximumLength(100).WithErrorCode(ValidationErrorCode.TooLong);
+    }
+}
