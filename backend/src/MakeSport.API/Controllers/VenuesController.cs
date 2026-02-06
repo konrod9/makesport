774b57a285f98893a3b2e921378b9079adc33131
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 13:27:37 +0700

    feat: Add Create Venue endpoint

---
+++ backend/src/MakeSport.API/Controllers/VenuesController.cs
@@ -0,0 +1,32 @@
+﻿using MakeSport.API.Requests;
+using MakeSport.API.Responses;
+using MakeSport.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeSport.API.Controllers;
+
+[ApiController]
+[Route("venues")]
+public class VenuesController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(201, Type = typeof(CreateVenueRequest))]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(403)]
+    public async Task<IActionResult> CreateVenue(
+        [FromBody] CreateVenueRequest request,
+        [FromServices] CreateVenueUseCase useCase,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateVenueCommand(request.Name, request.Description);
+        var venue = await useCase.Handle(command, cancellationToken);
+        
+        // TODO: поменять на CreateAtRoute после того как будет добавлен GetVenue
+        return Ok(new CreateVenueResponse()
+        {
+            Id = venue.Id,
+            Name = venue.Name,
+            Description = venue.Description
+        });
+    }
+}
---
+++ backend/src/MakeSport.API/Responses/CreateVenueResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MakeSport.API.Responses;
+
+public class CreateVenueResponse
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+}
