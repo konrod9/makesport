df30e95be3c10641c6a87a57b79ce5ec734911c7
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 13:54:30 +0700

    fix: Add register in Program

--- backend/src/MakeSport.API/Program.cs
+++ backend/src/MakeSport.API/Program.cs
@@ -1,3 +1,4 @@
+using MakeSport.Application.DI;
 using MakeSport.Infrastructure.DI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddVenuesServices();
 builder.Services.AddVenueStorage(builder.Configuration.GetConnectionString("Postgres"));
 
 var app = builder.Build();
