e18a1369436844ab49a3bfc03b3f8453f42c7aed
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 11:47:07 +0700

    chore: Add applying entity configurations from assemble

--- backend/src/MakeSport.Infrastructure.Postgres/VenueDbContext.cs
+++ backend/src/MakeSport.Infrastructure.Postgres/VenueDbContext.cs
@@ -6,4 +6,9 @@ namespace MakeSport.Infrastructure.Postgres;
 public class VenueDbContext(DbContextOptions<VenueDbContext> options) : DbContext(options)
 {
     public DbSet<Venue> Venue { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(VenueDbContext).Assembly);
+    }
 }
