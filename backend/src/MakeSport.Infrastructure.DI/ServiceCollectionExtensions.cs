97450d342eb6c1106c0ea6a7e0206f347d6151a4
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 11:37:57 +0700

    feat: add DI registration for db context

---
+++ backend/src/MakeSport.Infrastructure.DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MakeSport.Infrastructure.Postgres;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MakeSport.Infrastructure.DI;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddVenueStorage(this IServiceCollection services, string? dbConnectionString)
+    {
+        services
+            .AddDbContextPool<VenueDbContext>(options => options.UseNpgsql(dbConnectionString));
+        
+        return services;
+    }
+}
