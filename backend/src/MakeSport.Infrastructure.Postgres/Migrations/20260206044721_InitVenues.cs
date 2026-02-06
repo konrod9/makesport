6f0a2deba0cb214138219525dc973c7c259195e4
Rodion <rodion.kondrashin@bk.ru>
2026-02-06 11:49:10 +0700

    migrations: add init migration

---
+++ backend/src/MakeSport.Infrastructure.Postgres/Migrations/20260206044721_InitVenues.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MakeSport.Infrastructure.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitVenues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "venues",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_venues", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "venues");
+        }
+    }
+}
