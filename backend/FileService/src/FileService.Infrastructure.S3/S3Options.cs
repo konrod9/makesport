84b49cf75cadd4e7ca719b495169478283228720
Rodion.Kondrashin <rodion.kondrashin17@gmail.com>
2026-05-28 12:59:56 +0700

    feat: Добавлены параметры для управления загрузкой в S3 и ограничение параллельных запросов

--- backend/FileService/src/FileService.Infrastructure.S3/S3Options.cs
+++ backend/FileService/src/FileService.Infrastructure.S3/S3Options.cs
@@ -13,4 +13,8 @@ public record S3Options
     public int DownloadUrlExpirationHours { get; init; } = 24;
     
     public IReadOnlyList<string> RequiredBuckets { get; init; } = Array.Empty<string>();
+    
+    public double UploadUrlExpirationHours { get; init; } = 1;
+    
+    public int MaxConcurrentRequests { get; init; } = 50;
 }
--- backend/FileService/src/FileService.Infrastructure.S3/S3Provider.cs
+++ backend/FileService/src/FileService.Infrastructure.S3/S3Provider.cs
@@ -3,7 +3,6 @@ using Amazon.S3.Model;
 using CSharpFunctionalExtensions;
 using FileService.Application;
 using FileService.Domain.Shared;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -15,17 +14,21 @@ public class S3Provider : IS3Provider
     private readonly S3Options _s3Options;
     private readonly ILogger<S3Provider> _logger;
 
+    private readonly SemaphoreSlim _requestsSemaphore;
+
     public S3Provider(IAmazonS3 s3Client, IOptions<S3Options> s3Options, ILogger<S3Provider> logger)
     {
         _s3Client = s3Client;
         _logger = logger;
         _s3Options = s3Options.Value;
+        _requestsSemaphore = new SemaphoreSlim(_s3Options.MaxConcurrentRequests);
     }
 
     public async Task<Result<string, Error>> StartMultipartUploadAsync(
         string bucketName,
         string key,
-        string contentType)
+        string contentType,
+        CancellationToken ct)
     {
         try
         {
@@ -36,7 +39,7 @@ public class S3Provider : IS3Provider
                 ContentType = contentType
             };
 
-            var response = await _s3Client.InitiateMultipartUploadAsync(request);
+            var response = await _s3Client.InitiateMultipartUploadAsync(request, ct);
 
             return response.UploadId;
         }
@@ -46,6 +49,46 @@ public class S3Provider : IS3Provider
                 bucketName, key, ex.Message);
             return S3ErrorMapper.ToError(ex);
         }
+    }
+    
+    public async Task<Result<IReadOnlyList<string>, Error>> GenerateAllChunksUploadUrlsAsync(
+        string bucketName,
+        string key,
+        string uploadId,
+        int totalChunks,
+        CancellationToken ct)
+    {
+        var tasks = Enumerable.Range(0, totalChunks)
+            .Select(async partNumber =>
+            {
+                await _requestsSemaphore.WaitAsync(ct);
+
+                try
+                {
+                    var request = new GetPreSignedUrlRequest
+                    {
+                        BucketName = bucketName,
+                        Key = key,
+                        Verb = HttpVerb.PUT,
+                        UploadId = uploadId,
+                        PartNumber = partNumber,
+                        Expires = DateTime.UtcNow.AddHours(_s3Options.UploadUrlExpirationHours),
+                        Protocol = _s3Options.WithSsl ? Protocol.HTTPS : Protocol.HTTP
+                    };
+
+                    var url = await _s3Client.GetPreSignedURLAsync(request);
+
+                    return url;
+                }
+                finally
+                {
+                    _requestsSemaphore.Release();
+                }
+            });
+        
+        var results = await Task.WhenAll(tasks);
+
+        return results;
     }
 
     public async Task<string> GenerateDownloadUrlAsync(string bucketName, string key)
