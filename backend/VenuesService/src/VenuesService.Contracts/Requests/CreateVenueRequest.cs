2f97c735d86404d91fdc8c51fcd826ccb4703253
Rodion.Kondrashin <rodion.kondrashin17@gmail.com>
2026-06-20 13:42:55 +0700

    back(venues): Update create venue use case

--- backend/VenuesService/src/VenuesService.Contracts/Requests/CreateVenueRequest.cs
+++ backend/VenuesService/src/VenuesService.Contracts/Requests/CreateVenueRequest.cs
@@ -2,4 +2,25 @@ using VenuesService.Contracts.Dtos;
 
 namespace VenuesService.Contracts.Requests;
 
-public record CreateVenueRequest(string Title, string? Description, AddressDto Address, CoordinatesDto Coordinates);
+public record CreateVenueRequest
+{
+    public string Title { get; init; }
+    
+    public string? Description { get; init; }
+    
+    public AddressDto Address { get; init; }
+    
+    public CoordinatesDto Coordinates { get; init; }
+    
+    public string SportType { get; init; } = string.Empty;
+    
+    public string Surface { get; init; } = string.Empty;
+    
+    public bool IsOpen { get; init; }
+    
+    public bool HasLighting { get; init; }
+    
+    public bool IsFree { get; init; }
+    
+    public string? WorkingHours { get; init; }
+}
--- backend/VenuesService/src/VenuesService.Domain/Venues/Venue.cs
+++ backend/VenuesService/src/VenuesService.Domain/Venues/Venue.cs
@@ -9,13 +9,34 @@ public class Venue
     {
     }
 
-    private Venue(VenueId id, string title, string? description, Address address, Coordinates coordinates)
+    private Venue(
+        VenueId id, 
+        string title, 
+        string? description, 
+        Address address, 
+        Coordinates coordinates,
+        bool isOpen, 
+        bool hasLighting,
+        bool isFree, 
+        string? workingHours, 
+        double rating, 
+        int reviewCount,
+        string surface, 
+        string sportType)
     {
         Id = id;
         Title = title;
         Description = description;
         Address = address;
         Coordinates = coordinates;
+        IsOpen = isOpen;
+        HasLighting = hasLighting;
+        IsFree = isFree;
+        WorkingHours = workingHours;
+        Rating = rating;
+        ReviewCount = reviewCount;
+        Surface = surface;
+        SportType = sportType;
     }
 
     public VenueId Id { get; private set; }
@@ -23,18 +44,18 @@ public class Venue
     public string Title { get; private set; }
 
     public string? Description { get; private set; }
-    
+
     public bool IsOpen { get; private set; }
     public bool HasLighting { get; private set; }
     public bool IsFree { get; private set; }
 
     public string? WorkingHours { get; private set; }
-    
+
     public double Rating { get; private set; }
     public int ReviewCount { get; private set; }
-    
+
     public string SportType { get; private set; }
-    
+
     public string Surface { get; private set; }
 
     public Address Address { get; private set; }
@@ -43,8 +64,34 @@ public class Venue
 
     public Guid VideoId { get; private set; }
 
-    public static Venue Create(VenueId id, string title, string? description, Address address, Coordinates location)
+    public static Venue Create(
+        VenueId id, 
+        string title,
+        string? description,
+        Address address, 
+        Coordinates location,
+        bool isOpen, 
+        bool hasLighting,
+        bool isFree, 
+        string? workingHours, 
+        string surface, 
+        string sportType)
     {
-        return new Venue(id, title, description, address, location);
+        var rating = 0;
+        var reviewCount = 0;
+        
+        return new Venue(id, 
+            title,
+            description, 
+            address,
+            location, 
+            isOpen, 
+            hasLighting,
+            isFree,
+            workingHours,
+            rating,
+            reviewCount,
+            surface,
+            sportType);
     }
 }
